e29c440d59d70696b1e0e90c55b9cc3949c1cabc
Boris <boristul97@gmail.com>
2020-03-08 02:14:02 +0300

    Add uikit module

---
+++ uikit/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        versionCode = 1
+        versionName = "0.0.0"
+
+        minSdkVersion(21)
+        targetSdkVersion(29)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
+    api("com.google.android.material:material:1.2.0-alpha05")
+}
