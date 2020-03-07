c7e2738ddf276ca8298b5b8e9a3d1a921fd89a21
Boris <boristul97@gmail.com>
2020-03-07 16:08:39 +0300

    Rebase new develop and fix conflicts

--- app/src/main/java/com/sports/sportstatistics/ui/main/athletes/AthletesFragment.kt
+++ app/src/main/java/com/sports/sportstatistics/ui/main/athletes/AthletesFragment.kt
@@ -1,21 +1,39 @@
 package com.sports.sportstatistics.ui.main.athletes
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.sports.sportstatistics.R
+import com.sports.sportstatistics.databinding.FragmentAthletesBinding
 import kotlinx.coroutines.launch
 
 class AthletesFragment : Fragment(R.layout.fragment_athletes) {
+    private var _binding: FragmentAthletesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAthletesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewModel by viewModels<AthletesFragmentViewModel>()
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            view.findViewById<TextView>(R.id.fi_info_tv).text =
-                viewModel.getAthletes().joinToString("\n\n")
+        lifecycleScope.launch {
+            binding.info.text = viewModel.getAthletes().toString()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/sports/sportstatistics/ui/main/teams/TeamsFragment.kt
+++ app/src/main/java/com/sports/sportstatistics/ui/main/teams/TeamsFragment.kt
@@ -1,21 +1,39 @@
 package com.sports.sportstatistics.ui.main.teams
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.sports.sportstatistics.R
+import com.sports.sportstatistics.databinding.FragmentTeamsBinding
 import kotlinx.coroutines.launch
 
 class TeamsFragment : Fragment(R.layout.fragment_teams) {
+    private var _binding: FragmentTeamsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTeamsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewModel by viewModels<TeamsFragmentViewModel>()
 
         viewLifecycleOwner.lifecycleScope.launch {
-            view.findViewById<TextView>(R.id.fn_text_tv).text = viewModel.getTeam(1).toString()
+            binding.fnTextTv.text = viewModel.getTeam(1).toString()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
